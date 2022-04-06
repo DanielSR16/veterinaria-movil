375bb472a6d941a5d2741d24b1401f5d9ad038be
DANIEL SANCHEZ ROBLES <danielsan.robles16@gmail.com>
2022-04-06 00:43:16 -0500

    modificado services

--- pet_aplication/lib/services/loginService.dart
+++ pet_aplication/lib/services/loginService.dart
@@ -212,3 +212,90 @@ Future<List<dynamic>> deleteMascota(Mascota mascota, String token) async {
     return ['Error en la respuesta'];
   }
 }
+
+Future<List<dynamic>> get_citas_all(String token) async {
+  var resultado;
+  print(LoginProvider().jwt);
+  print('object');
+  try {
+    final response =
+        await http.get(Uri.http(ip + ':9999', '/listCita'), headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: token
+    });
+
+    // body: json.encode({"username": usuario, "password": password}));
+    resultado = json.decode(response.body);
+    print(resultado);
+    return resultado;
+  } catch (e) {
+    return ['Error en la respuesta'];
+  }
+}
+
+Future<List<dynamic>> updateCitas(Citas citas, String token) async {
+  try {
+    final response = await http.post(
+      Uri.http(ip + ':9999', '/cita/update'),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: token
+      },
+      body: json.encode({
+        "idCita": citas.idCita,
+        "fecha": citas.fecha,
+        "hora": citas.hora,
+        "tipoServicio": citas.tipoServicio,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print(data);
+      if (data == null) {
+        return [];
+      } else {
+        return data;
+      }
+    } else {
+      return ['No se ha podido conectar al servidor'];
+    }
+  } catch (e) {
+    return ['Error en la respuesta'];
+  }
+}
+
+Future<List<dynamic>> deleteCitas(Citas citas, String token) async {
+  try {
+    final response = await http.post(
+      Uri.http(ip + ':9999', '/cita/deletes'),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: token
+      },
+      body: json.encode(
+        {
+          "idCita": citas.idCita,
+          "fecha": citas.fecha,
+          "hora": citas.hora,
+          "tipoServicio": citas.tipoServicio,
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print('a');
+      print(data);
+      if (data == null) {
+        return [];
+      } else {
+        return data;
+      }
+    } else {
+      return ['No se ha podido conectar al servidor'];
+    }
+  } catch (e) {
+    return ['Error en la respuesta'];
+  }
+}
